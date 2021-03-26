474ff65b140b4eb0fe2195fae5680ab43f4649bc
Aftaab Dobani <1234dobani@gmail.com>
2021-03-26 16:35:31 -0400

    fixed all displays and now working on local storage

--- assests/script.js
+++ assests/script.js
@@ -1,11 +1,22 @@
-//DOM Variables 
+//Header Date
 var currentDayMonthSpan = $("#currentDay");
 
 function updateCurrentDate(){
-    currentDayMonthSpan.text(moment().format("dddd, MMMM d"));
+    currentDayMonthSpan.text(moment().format("dddd, MMMM Do YYYY"));
 }
 
-console.log(moment().format("dddd, MMMM d"));
+//Splits and hour and time ID
+function timeBlocks() {
+    $(".time-block").each(function() {
+        console.log($(this).attr("id").split("-")[1]);
+    });
+}
+
+//Save button
+$(".saveBtn").on("click", function(){
+    
+}
 
 //Function 
 updateCurrentDate();
+timeBlocks(); 
