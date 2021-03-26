f8c510a1c8abfaba46de029709c7fa3cd11d949b
Aftaab Dobani <1234dobani@gmail.com>
2021-03-26 02:10:17 -0400

    added moment for the header

--- assests/script.js
+++ assests/script.js
@@ -1,1 +1,11 @@
-console.log(moment().format("")
+//DOM Variables 
+var currentDayMonthSpan = $("#currentDay");
+
+function updateCurrentDate(){
+    currentDayMonthSpan.text(moment().format("dddd, MMMM d"));
+}
+
+console.log(moment().format("dddd, MMMM d"));
+
+//Function 
+updateCurrentDate();
