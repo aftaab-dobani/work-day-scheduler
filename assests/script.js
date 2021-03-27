d40f4631e534dbd3abade9a8ed28bfbe636fb281
Aftaab Dobani <1234dobani@gmail.com>
2021-03-27 13:14:27 -0400

    added variables for the buttons

--- assests/script.js
+++ assests/script.js
@@ -1,22 +1,33 @@
-//Header Date
+//Variable
+var userInput = $("#description");
 var currentDayMonthSpan = $("#currentDay");
 
+//Button Var : created to insert into on click function 
+var button1 = $("#saveBtn1");
+var button2 = $("#saveBtn2");
+var button3 = $("#saveBtn3");
+var button4 = $("#saveBtn4");
+var button5 = $("#saveBtn5");
+var button6 = $("#saveBtn6");
+var button7 = $("#saveBtn7");
+var button8 = $("#saveBtn8");
+var button9 = $("#saveBtn9");
+
+//Header Date
 function updateCurrentDate(){
     currentDayMonthSpan.text(moment().format("dddd, MMMM Do YYYY"));
-}
-
-//Splits and hour and time ID
-function timeBlocks() {
-    $(".time-block").each(function() {
-        console.log($(this).attr("id").split("-")[1]);
-    });
-}
-
-//Save button
-$(".saveBtn").on("click", function(){
     
 }
 
+//Save button & Local Storage 
+//1.function to save the description
+
+button1.on("click", function(){
+    console.log('button clicked');
+})
+
+
+
 //Function 
 updateCurrentDate();
-timeBlocks(); 
+
