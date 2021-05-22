f0a85d0d75b0b4762fed490048f5ae28e3f69a1e
Aftaab Dobani <1234dobani@gmail.com>
2021-05-22 01:28:31 -0400

    added hour updater function

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,38 @@
 $(document).ready(function () {
-    $('.saveBtn').on('click', function () {
-        var value = $(this).siblings('.description').val();
-        var time = $(this).parent().attr('id');
+  $(".saveBtn").on("click", function () {
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-        // saving to local storage & showing
-        localStorage.setItem(time, value); 
-        $('.notfications').addClass('show'); 
+    // saving to local storage & showing
+    localStorage.setItem(time, value);
+    $(".notfications").addClass("show");
 
-        setTimeout(function () {
-            $('.notification').addClass('show');
-        }, 5000);
-    })
-})
+    setTimeout(function () {
+      $(".notification").addClass("show");
+    }, 5000);
+  });
 
-//Button Var : created to insert into on click function 
+  function hourUpdater() {
+    var CurrentHour = moment().hours();
+
+    $("time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-"[1]));
+
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+      }
+    });
+  }
+});
+
+//Button Var : created to insert into on click function
 var button1 = $("#saveBtn1");
 var button2 = $("#saveBtn2");
 var button3 = $("#saveBtn3");
@@ -24,47 +43,36 @@ var button7 = $("#saveBtn7");
 var button8 = $("#saveBtn8");
 var button9 = $("#saveBtn9");
 
-
-  
-
-
-
 //Header Date
-function updateCurrentDate(){
-    currentDayMonthSpan.text(moment().format("dddd, MMMM Do YYYY"));
+function updateCurrentDate() {
+  currentDayMonthSpan.text(moment().format("dddd, MMMM Do YYYY"));
 }
 console.log("showing time");
 
-button1.on("click", function(){
-    console.log("button clicked")
+button1.on("click", function () {
+  console.log("button clicked");
 });
 
-//Color Change 
-timeBlock.each(function(i, element){
-    //var hour = replace  (incomplete)
-    if (hour===CurrentHour) {
-        $(element).addClass("present")
-    } else if (hour > CurrentHour) {
-        $(element).addClass("")
-    }
-})
+//Color Change
+timeBlock.each(function (i, element) {
+  //var hour = replace  (incomplete)
+  if (hour === CurrentHour) {
+    $(element).addClass("present");
+  } else if (hour > CurrentHour) {
+    $(element).addClass("");
+  }
+});
 
-
-//Save button & Local Storage 
+//Save button & Local Storage
 //1.function to save the description
-button1.on("click", function(){
-    var value = userInput1.value
-    localStorage.setItem("print", value)
-    var secured = localStorage.getItem("print")
-    console.log(clicked);
-    
-})
-
+button1.on("click", function () {
+  var value = userInput1.value;
+  localStorage.setItem("print", value);
+  var secured = localStorage.getItem("print");
+  console.log(clicked);
+});
 
 $("9AM .description").val(localStorage.getItem("9AM"));
 
-
-//Function 
+//Function
 updateCurrentDate();
-
-
